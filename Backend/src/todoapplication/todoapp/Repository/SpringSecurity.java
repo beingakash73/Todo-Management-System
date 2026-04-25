80a063133f69921e35b2bbe17208c44084ef3efe
beingakash73 <kucheakash@gmail.com>
2026-04-25 18:41:13 +0530

    Add files via upload

---
+++ Backend/src/todoapplication/todoapp/Repository/SpringSecurity.java
@@ -0,0 +1,32 @@
+//package com.todoAPI.TodoApplication.basic;
+//
+//
+////import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//
+//@Configuration
+//public class SpringSecurity {
+//
+//@Bean
+//public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//	return
+//			http.authorizeHttpRequests(auth -> auth
+//					.requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+//					.anyRequest().authenticated())
+//			.httpBasic(Customizer.withDefaults())
+//			.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//			.csrf(csrf ->csrf.disable())
+//			.build();
+//	
+//}
+//
+//}
