222c3eb1186827755ca1b903906895306f1e9ea7
beingakash73 <kucheakash@gmail.com>
2026-04-25 13:55:15 +0530

    Add files via upload

---
+++ FrontEnd/ReactApp/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import './Component/Todo/TodoApp.css';
+import TodoApp from './Component/Todo/TodoApp';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
