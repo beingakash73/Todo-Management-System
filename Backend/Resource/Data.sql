117f2982d5d05a8f1879aff49ccde69608ad15d9
beingakash73 <kucheakash@gmail.com>
2026-04-25 18:39:28 +0530

    Add files via upload

---
+++ Backend/Resource/Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO todos (id, username, description, target_date, done) VALUES
+(10001, 'Deogiri', 'Learn Spring Boot JPA', '2026-03-25', false),
+(10002, 'Deogiri', 'Learn Spring Data JPA', '2026-03-26', false),
+(10003, 'Deogiri', 'Learn Spring REST API', '2026-03-27', false);
